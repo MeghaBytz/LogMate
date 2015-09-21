a36c943af943016c169af2890ab3ffe9fb3860ad
Jared Friese <jared.friese@gmail.com>
2015-09-20 20:23:16 -0700

    Create Xcode project

---
+++ LogMate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LogMate
+//
+//  Created by Jared Friese on 9/20/15.
+//  Copyright © 2015 j&m. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
